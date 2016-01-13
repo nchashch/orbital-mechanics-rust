0cea51cfad06e7fb73a6cce1d9b9ccad5cf8eda0
Nikita Chashchinskii <nikita.chashinsky@yandex.ru>
2016-01-13 22:50:43 +0300

    Don't recompute the rotation matrix for every to_csv call

--- src/koe.rs
+++ src/koe.rs
@@ -15,25 +15,42 @@ pub struct KOE {
     pub ap: f64,
     pub m0: f64, // Mean anomaly
     pub cb: Rc<CentralBody>,
+    pub to_csv_rot: Rot3<f64>,
 }
 
 impl Tick for KOE {
     fn tick(&self, dt: f64) -> Self {
         let n = (self.cb.mu/self.a.powf(3.0)).sqrt();
-        KOE::new(
-            self.a,
-            self.e,
-            self.inc,
-            self.lan,
-            self.ap,
-            self.m0 + n * dt,
-            self.cb.clone()
-        )
+        KOE {
+            a: self.a,
+            e: self.e,
+            inc: self.inc,
+            lan: self.lan,
+            ap: self.ap,
+            m0: self.m0 + n * dt,
+            cb: self.cb.clone(),
+            to_csv_rot: self.to_csv_rot,
+        }
     }
 }
 
 impl KOE {
     pub fn new(a: f64, e: f64, inc: f64, lan: f64, ap: f64, m0: f64, cb: Rc<CentralBody>) -> KOE {
+        let mut rot = Rot3::new_identity(3);
+        if approx_eq(&inc, &0.0) {
+            if !approx_eq(&e, &0.0) {
+                rot = Rot3::new(cb.up * ap);
+            }
+        } else {
+            let lan_axisangle = cb.up * lan;
+            let inc_axisangle = cb.reference * inc;
+            let ap_axisangle = cb.up * ap;
+            rot = Rot3::new(lan_axisangle);
+            rot = rot.prepend_rotation(&inc_axisangle);
+            if !approx_eq(&e, &0.0) {
+                rot = rot.prepend_rotation(&ap_axisangle);
+            }
+        }
         KOE {
             a: a,
             e: e,
@@ -42,6 +59,7 @@ impl KOE {
             ap: ap,
             m0: m0,
             cb: cb,
+            to_csv_rot: rot,
         }
     }
     
@@ -76,23 +94,8 @@ impl KOE {
         let mut r = (self.cb.reference*ta.cos() + self.cb.right*ta.sin()) * dist;
         let mut v = (self.cb.reference*(-ea.sin()) +
                     self.cb.right*((1.0-self.e.powf(2.0)).sqrt()*ea.cos())) * ((self.cb.mu*self.a).sqrt()/dist);
-        let mut rot = Rot3::new_identity(3);
-        if approx_eq(&self.inc, &0.0) {
-            if !approx_eq(&self.e, &0.0) {
-                rot = Rot3::new(self.cb.up * self.ap);
-            }
-        } else {
-            let lan_axisangle = self.cb.up * self.lan;
-            let inc_axisangle = self.cb.reference * self.inc;
-            let ap_axisangle = self.cb.up * self.ap;
-            rot = Rot3::new(lan_axisangle);
-            rot = rot.prepend_rotation(&inc_axisangle);
-            if !approx_eq(&self.e, &0.0) {
-                rot = rot.prepend_rotation(&ap_axisangle);
-            }
-        }
-        r = rot.transform(&r);
-        v = rot.transform(&v);
+        r = self.to_csv_rot.transform(&r);
+        v = self.to_csv_rot.transform(&v);
         CSV::new(r, v, self.cb.clone())
     }
 
